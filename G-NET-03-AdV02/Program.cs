6880e4f81e84b68cb60427ed5f9d638b247d2a1b
AhmedlSaied <kledkled.17@gmail.com>
2026-07-25 08:11:20 +0300

    Starter Code: Data Models & Product Catalog

--- G-NET-03-AdV02/Program.cs
+++ G-NET-03-AdV02/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopMaster
+{
+    // Starter Code: Data Models & Product Catalog
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty; // "Electronics", "Clothing", "Food", "Books"
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
